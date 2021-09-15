d65f4838ac0c1e11093e94c963190060e885673a
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-15 09:57:24 -0500

    Starting with components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () => {
-	return (
-		<div>
-			<p>Testing</p>
-			<p>Testing</p>
-		</div>
-	)
-}
+import Join from './components/Join';
+import Chat from './components/Chat';
 
-export default App
+const App = () => (
+	<Router>
+		<Route path="/" exact component={Join} />
+		<Route path="/chat" exact component={Chat} />
+	</Router>
+);
+
+export default App;
---
+++ client/src/components/Chat.tsx
@@ -0,0 +1,10 @@
+const Chat = () => {
+	return (
+		<div>
+			<p>Chat</p>
+			<p></p>
+		</div>
+	);
+};
+
+export default Chat;
