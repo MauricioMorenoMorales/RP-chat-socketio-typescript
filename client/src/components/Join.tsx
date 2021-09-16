0bbc589c3a10c7659e879dd719011cf3cf814a7d
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-16 13:56:07 -0500

    Starting onChange functions

--- client/src/components/Join.tsx
+++ client/src/components/Join.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+	const [name, setName] = useState('');
+	const [room, setRoom] = useState('');
+
 	return (
-		<div>
-			<p>Join</p>
-			<p></p>
+		<div className="joinOuterContainer">
+			<div className="joinInnerContainer">
+				<h1 className="heading">Join</h1>
+				<div>
+					<input placeholder="Name" type="text" className="joinInput" />
+				</div>
+				<div>
+					<input placeholder="Room" type="text" className="joinInput mt-20" />
+				</div>
+				<Link to="/">
+					<button className="button mt-20" type="submit">
+						Sign In
+					</button>
+				</Link>
+			</div>
 		</div>
 	);
 };
