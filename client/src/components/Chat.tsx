d9cdfcb8416ef8d533eb9f4c4103ebb5597c2e71
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-16 21:19:20 -0500

    Server client integration added

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,4 +1,26 @@
-const Chat = () => {
+import { useEffect, useState } from 'react';
+import queryString from 'query-string';
+import io, { Socket } from 'socket.io-client';
+
+let socket: Socket;
+
+const Chat: React.FC<{ location: Location }> = ({ location }) => {
+	const [name, setName] = useState<string>('');
+	const [room, setRoom] = useState<string>('');
+	const ENDPOINT = 'localhost:5000';
+
+	useEffect(() => {
+		const { name, room } = queryString.parse(location.search);
+		socket = io(ENDPOINT);
+		setName(String(name));
+		setRoom(String(room));
+		socket.emit('join', { name, room }, () => {});
+		return () => {
+			socket.emit('disconnect');
+			socket.off();
+		};
+	}, [ENDPOINT, location.search]);
+
 	return (
 		<div>
 			<p>Chat</p>
