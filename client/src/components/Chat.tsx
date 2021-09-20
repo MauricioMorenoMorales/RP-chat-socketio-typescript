cfb3e1e79b4ee3d172355ccece4b55c7c8f74396
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-20 13:05:08 -0500

    Repairing server bug

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import io, { Socket } from 'socket.io-client';
 
@@ -7,6 +7,8 @@ let socket: Socket;
 const Chat: React.FC<{ location: Location }> = ({ location }) => {
 	const [name, setName] = useState<string>('');
 	const [room, setRoom] = useState<string>('');
+	const [message, setMessage] = useState<string>('');
+	const [messages, setMessages] = useState<Array<string>>([]);
 	const ENDPOINT = 'localhost:5000';
 
 	useEffect(() => {
@@ -21,10 +23,32 @@ const Chat: React.FC<{ location: Location }> = ({ location }) => {
 		};
 	}, [ENDPOINT, location.search]);
 
+	useEffect(() => {
+		socket.on('message', message => {
+			setMessages([...messages, message]);
+		});
+	}, [messages]);
+
+	// function for sending messages
+	const sendMessage = (event: React.KeyboardEvent) => {
+		event.preventDefault();
+		if (message) socket.emit('sendMessage', message, () => setMessage(''));
+	};
+
+	console.log(message, messages);
+
 	return (
-		<div>
-			<p>Chat</p>
-			<p></p>
+		<div className="outerContainer">
+			<div className="container">
+				<input
+					value={message}
+					onChange={event => setMessage(event.target.value)}
+					onKeyPress={event =>
+						event.key === 'Enter' ? sendMessage(event) : null
+					}
+					type="text"
+				/>
+			</div>
 		</div>
 	);
 };
